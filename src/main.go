356ab8edb92b9b017953b64631e8dfa5266f325e
Pascal Sauer <email@cubexs.dev>
2022-12-10 03:17:59 +0100

    Initialize webserver

--- src/main.go
+++ src/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	var env = os.Getenv("ENV")
+	if env == "production" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":3000")
 }
